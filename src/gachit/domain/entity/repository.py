317097432d4ab94f4b0e75bbcc9f671f2afc64b7
umepon0626 <ryuichiastrona@gmail.com>
2024-08-04 14:40:16 +0900

    add Repository class

---
+++ src/gachit/domain/entity/repository.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from gachit.exception import EntiryError
+
+
+class Repository:
+    repository_root_dir: Path
+    git_dir: Path
+
+    def __init__(self, repository_root_dir: Path = Path.cwd()):
+        self.repository_root_dir = self.__get_repository_root(repository_root_dir)
+        self.git_dir = self.repository_root_dir / ".git"
+
+    def __get_repository_root(self, path: Path) -> Path:
+        git_dir = path / ".git"
+        if git_dir.exists():
+            return path
+
+        parent = path.parent
+        if parent == path:
+            raise EntiryError("Not a git repository")
+
+        return self.__get_repository_root(parent)
---
+++ src/gachit/exception.py
@@ -0,0 +1,2 @@
+class EntiryError(Exception):
+    pass

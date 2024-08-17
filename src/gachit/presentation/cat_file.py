a5d729f842471a35a7b31f71feab881ec9ce64f5
umepon0626 <ryuichiastrona@gmail.com>
2024-08-17 14:24:10 +0900

    fix: undo unnecessary change

--- src/gachit/presentation/cat_file.py
+++ src/gachit/presentation/cat_file.py
@@ -1,5 +1,10 @@
-from gachit.domain.entity import Blob
+from gachit.domain.entity import Blob, Commit, Tree
 
 
-def cat_file_presentation(data: Blob) -> None:
-    print(data.data.decode("ascii"))
+def cat_file_presentation(data: Blob | Tree | Commit) -> None:
+    if isinstance(data, Blob):
+        print(data.data.decode("ascii"))
+    elif isinstance(data, Tree):
+        raise NotImplementedError("Tree is not supported yet")
+    else:
+        raise ValueError(f"Unknown data type: {data}")

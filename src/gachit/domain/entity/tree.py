dae08932d00a336b1eca2a3f539905f77f73e4e6
umepon0626 <ryuichiastrona@gmail.com>
2024-09-28 20:52:41 +0900

    feat: add tree diff service

--- src/gachit/domain/entity/tree.py
+++ src/gachit/domain/entity/tree.py
@@ -33,7 +33,7 @@ class Tree:
     """Tree
 
     Args:
-        directory (Path | None): Directory path. If the tree is for the root,
+        path (Path | None): Directory path. If the tree is for the root,
         it should be None.
 
         entries (list[TreeLeaf | Tree]): Entries
@@ -43,7 +43,7 @@ class Tree:
     """
 
     format: ClassVar[str] = "tree"
-    directory: Path
+    path: Path
     entries: list[Union[TreeLeaf, "Tree"]] = field(default_factory=list)
 
     def add_entry(self, entry: TreeLeaf) -> None:
@@ -52,7 +52,7 @@ class Tree:
         Args:
             entry (TreeLeaf): Entry to add
         """
-        relative_path = entry.path.relative_to(self.directory)
+        relative_path = entry.path.relative_to(self.path)
         if len(relative_path.parts) == 1:
             self.entries.append(entry)
         elif len(relative_path.parts) > 1:
@@ -60,12 +60,12 @@ class Tree:
             for e in self.entries:
                 if (
                     isinstance(e, self.__class__)
-                    and e.directory.name == relative_path.parts[0]
+                    and e.path.name == relative_path.parts[0]
                 ):
                     e.add_entry(entry=entry)
                     return
             # If not found, create a new tree
-            tree = self.__class__(directory=self.directory / relative_path.parts[0])
+            tree = self.__class__(path=self.path / relative_path.parts[0])
             tree.add_entry(entry=entry)
             self.entries.append(tree)
         else:
---
+++ src/tests/tree_diff_test.py
@@ -0,0 +1,91 @@
+from pathlib import Path
+
+from gachit.domain.entity import Mode, Sha, Tree, TreeLeaf
+from gachit.domain.service.diff import TreeDiffService
+
+
+def test_tree_diff() -> None:
+    """
+    Test the TreeDiffService.compare_trees method.
+
+    Before:
+    .
+    └── hoge
+        ├── README.md (sha: `89421b07810f80d5a6ae393587733a80a34661e2`)
+        ├── LICENSE.md (sha: `32bb9315c35a92995d0a5dbc2dd6179616202c0c`)
+        └── src (sha: `8d9ec2e2c951124ecf4bc56dcb491fb06a7d6e5a`)
+            └── main.py (sha: `3bbbd448a70bee0a672fc5502c28dfd8a6c88987`)
+
+    After:
+    .
+    └── hoge
+        ├── README.md (sha: `89421b07810f80d5a6ae393587733a80a34661e2`)
+        ├── LICENSE.md (sha: `467644d9095952223c6402a46ad545174d8bae12`)
+        └── src (sha: `15ea5656dcec2be3ec658b0fce5a88a50ea64a99`)
+            └── test.py (sha: `683db051e198700f73438e30ce503b774bb7ce2d`)
+
+    Expected:
+    TreeDiff([
+        BlobDiff(Path("hoge/LICENSE.md"), "32bb9315c35a92995d0a5dbc2dd6179616202c0c",
+        "467644d9095952223c6402a46ad545174d8bae12"),
+        BlobDiff(Path("hoge/src/main.py"), "3bbbd448a70bee0a672fc5502c28dfd8a6c88987",
+        None),
+        BlobDiff(Path("hoge/src/test.py"), None,
+        "683db051e198700f73438e30ce503b774bb7ce2d"),
+    ])
+    """
+
+    root_directory = Path("hoge")
+    readme = TreeLeaf(
+        Mode.FILE,
+        root_directory / "README.md",
+        Sha("89421b07810f80d5a6ae393587733a80a34661e2"),
+    )
+    before_license = TreeLeaf(
+        Mode.FILE,
+        root_directory / "LICENSE.md",
+        Sha("32bb9315c35a92995d0a5dbc2dd6179616202c0c"),
+    )
+    after_license = TreeLeaf(
+        Mode.FILE,
+        root_directory / "LICENSE.md",
+        Sha("467644d9095952223c6402a46ad545174d8bae12"),
+    )
+    main_py = TreeLeaf(
+        Mode.FILE,
+        root_directory / "src" / "main.py",
+        Sha("3bbbd448a70bee0a672fc5502c28dfd8a6c88987"),
+    )
+    test_py = TreeLeaf(
+        Mode.FILE,
+        root_directory / "src" / "test.py",
+        Sha("683db051e198700f73438e30ce503b774bb7ce2d"),
+    )
+
+    before = Tree(root_directory, [])
+    before.add_entry(readme)
+    before.add_entry(before_license)
+    before.add_entry(main_py)
+
+    after = Tree(root_directory, [])
+    after.add_entry(readme)
+    after.add_entry(after_license)
+    after.add_entry(test_py)
+    tree_diff_service = TreeDiffService()
+    tree_diff_service.compare_trees(before, after)
+    assert tree_diff_service.diff.blob_diffs[Path("hoge/LICENSE.md")].before == Sha(
+        "32bb9315c35a92995d0a5dbc2dd6179616202c0c"
+    )
+    assert tree_diff_service.diff.blob_diffs[Path("hoge/LICENSE.md")].after == Sha(
+        "467644d9095952223c6402a46ad545174d8bae12"
+    )
+
+    assert tree_diff_service.diff.blob_diffs[Path("hoge/src/main.py")].before == Sha(
+        "3bbbd448a70bee0a672fc5502c28dfd8a6c88987"
+    )
+    assert tree_diff_service.diff.blob_diffs[Path("hoge/src/main.py")].after is None
+
+    assert tree_diff_service.diff.blob_diffs[Path("hoge/src/test.py")].before is None
+    assert tree_diff_service.diff.blob_diffs[Path("hoge/src/test.py")].after == Sha(
+        "683db051e198700f73438e30ce503b774bb7ce2d"
+    )
